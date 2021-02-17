1190321cbc9f3e3d7d80d40634e09b4c8d0fdae6
Arseniy <plakhotniuk.av@phystech.edu>
2021-02-17 22:38:31 +0300

    draw 1 update

--- Lab2/My_Draw_1.py
+++ Lab2/My_Draw_1.py
@@ -38,56 +38,117 @@ moon_radius = (100)
 draw_circle(color_white, moon_coord, moon_radius, 0)
 
 
-def draw_ellipse(surface, color, coord_rect, wid):
-    pygame.draw.ellipse(surface=surface, color=color, rect=coord_rect, width=wid)
+def draw_ellipse(Surface, color, coord_rect, wid):
+    pygame.draw.ellipse(surface=Surface, color=color, rect=coord_rect, width=wid)
 
 
 
 # Draw Clouds
+
+def Clouds(number_of_clouds, color, size):
+    for i in range(number_of_clouds):
+        coord_cloud = (random.randint(0, screen_size[0]), random.randint(0, screen_size[1]//3))
+        rect_cloud = (coord_cloud[0], coord_cloud[1], size[0], size[1])
+        draw_ellipse(screen, color, rect_cloud, 0)
+        i += 1
+
 dark_cloud_size = (400, 70)
 light_cloud_size = (500, 90)
 number_of_dark_clouds = 3
 number_of_light_clouds = 5
+Clouds(number_of_dark_clouds, color_dark_grew_cloud, dark_cloud_size)
+Clouds(number_of_light_clouds, color_light_grey_cloud, light_cloud_size)
 
-for i in range(number_of_light_clouds):
-    coord_cloud = (random.randint(0, screen_size[0]), random.randint(0, screen_size[1]//3))
-    rect_cloud = (coord_cloud[0], coord_cloud[1], light_cloud_size[0], light_cloud_size[1])
-    print(rect_cloud)
-    draw_ellipse(screen, color_light_grey_cloud, rect_cloud, 0)
-    i += 1
-for i in range(number_of_dark_clouds):
-    coord_cloud = (random.randint(0, screen_size[0]), random.randint(0, screen_size[1]//3))
-    rect_cloud = (coord_cloud[0], coord_cloud[1], dark_cloud_size[0], dark_cloud_size[1])
-    print(rect_cloud)
-    draw_ellipse(screen, color_dark_grew_cloud, rect_cloud, 0)
-    i += 1
 
-# SPACESHIP
+# SPACESHIPS
+def Spaceship(Spaceship_location, coef):
+    # Spaceshiplight
 
-# Spaceshiplight
-Spaceship_position = (150, 300)
-Spaceship_surface = pygame.surface.Surface(screen.get_size())
-pygame.draw.polygon(Spaceship_surface, color_white, [(Spaceship_position[0] - 100, Spaceship_position[1] + 300),
-                                                     Spaceship_position,
-                                                     (Spaceship_position[0] + 100, Spaceship_position[1] + 300)])
-Spaceship_surface.set_alpha(150)
-screen.blit(Spaceship_surface, (0, 0))
+    Spaceship_surface = pygame.surface.Surface(screen.get_size())
+    pygame.draw.polygon(Spaceship_surface, color_white, [((Spaceship_location[0] - 100)*coef, (Spaceship_location[1] + 300)*coef),
+                                                         (Spaceship_location[0]*coef, Spaceship_location[1]*coef),
+                                                         ((Spaceship_location[0] + 100)*coef, (Spaceship_location[1] + 300)*coef)])
+    Spaceship_surface.set_alpha(150)
+    screen.blit(Spaceship_surface, (0, 0))
 
-# Draw Spaceship Board and Cabin
-spaceship_board_coord_and_size = (Spaceship_position[0] - 145, Spaceship_position[1] - 10, 300, 90)
-draw_ellipse(screen, color_spaceship2, spaceship_board_coord_and_size, 0)
+    # Draw Spaceship Board and Cabin
+    spaceship_board_coord_and_size = ((Spaceship_location[0] - 145)*coef, (Spaceship_location[1] - 10)*coef, 300*coef, 90*coef)
+    draw_ellipse(screen, color_spaceship2, spaceship_board_coord_and_size, 0)
 
-spaceship_Cabin_coord_and_size = (Spaceship_position[0] - 100, Spaceship_position[1] - 20, 200, 70)
-draw_ellipse(screen, color_spaceship1, spaceship_Cabin_coord_and_size, 0)
+    spaceship_Cabin_coord_and_size = ((Spaceship_location[0] - 100)*coef, (Spaceship_location[1] - 20)*coef, 200*coef, 70*coef)
+    draw_ellipse(screen, color_spaceship1, spaceship_Cabin_coord_and_size, 0)
 
-# SpaceshipWindows
-draw_ellipse(screen, color_white, (Spaceship_position[0] - 130, Spaceship_position[1] + 30, 35, 12), 0)
-draw_ellipse(screen, color_white, (Spaceship_position[0] - 90, Spaceship_position[1] + 50, 35, 12), 0)
-draw_ellipse(screen, color_white, (Spaceship_position[0] - 40, Spaceship_position[1] + 60, 35, 12), 0)
-draw_ellipse(screen, color_white, (Spaceship_position[0] + 95, Spaceship_position[1] + 30, 35, 12), 0)
-draw_ellipse(screen, color_white, (Spaceship_position[0] + 65, Spaceship_position[1] + 50, 35, 12), 0)
-draw_ellipse(screen, color_white, (Spaceship_position[0] + 20, Spaceship_position[1] + 60, 35, 12), 0)
+    # SpaceshipWindows
+    draw_ellipse(screen, color_white, ((Spaceship_location[0] - 130)*coef, (Spaceship_location[1] + 30)*coef, 35*coef, 12*coef), 0)
+    draw_ellipse(screen, color_white, ((Spaceship_location[0] - 90)*coef, (Spaceship_location[1] + 50)*coef, 35*coef, 12*coef), 0)
+    draw_ellipse(screen, color_white, ((Spaceship_location[0] - 40)*coef, (Spaceship_location[1] + 60)*coef, 35*coef, 12*coef), 0)
+    draw_ellipse(screen, color_white, ((Spaceship_location[0] + 95)*coef, (Spaceship_location[1] + 30)*coef, 35*coef, 12*coef), 0)
+    draw_ellipse(screen, color_white, ((Spaceship_location[0] + 65)*coef, (Spaceship_location[1] + 50)*coef, 35*coef, 12*coef), 0)
+    draw_ellipse(screen, color_white, ((Spaceship_location[0] + 20)*coef, (Spaceship_location[1] + 60)*coef, 35*coef, 12*coef), 0)
 
+Spaceship1_location = (950, 600)
+Spaceship1_sim_coef = 0.5
+Spaceship2_location = (150, 400)
+Spaceship2_sim_coef = 0.8
+Spaceship(Spaceship1_location, Spaceship1_sim_coef)
+Spaceship(Spaceship2_location, Spaceship2_sim_coef)
+
+# Alien
+def alien_body_ellipse(location, Color, alien_bodypart_xcoord, alien_bodypart_ycoord, bodypart_angle, asize, bsize):
+    Alien_surface = pygame.Surface([300, 300], pygame.SRCALPHA)
+    pygame.draw.ellipse(Alien_surface, Color, (alien_bodypart_xcoord, alien_bodypart_ycoord,
+                                                     asize, bsize))
+    surface_rot = pygame.transform.rotate(Alien_surface, bodypart_angle)
+    screen.blit(surface_rot, location)
+def alien_body_arc(location, Color, alien_bodypart_xcoord, alien_bodypart_ycoord, bodypart_angle, asize, bsize, startang, endang, wid):
+    Alien_surface = pygame.Surface([300, 300], pygame.SRCALPHA)
+    pygame.draw.arc(Alien_surface, Color, (alien_bodypart_xcoord, alien_bodypart_ycoord,
+                                                     asize, bsize), startang, endang, wid)
+    surface_rot = pygame.transform.rotate(Alien_surface, bodypart_angle)
+    screen.blit(surface_rot, location)
+Alien_location = (300, 400)
+Alien_body_location = (100, 100)
+Alien_simillary_coef = 0.5
+# Main Body
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0], Alien_body_location[1], 0, 65, 125)
+# Left arm
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] - 10, Alien_body_location[1], 0, 35, 35)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] + 45, Alien_body_location[1], 0, 35, 35)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] - 33, Alien_body_location[1] + 20, 0, 27, 17)
+# Right arm
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] - 45, Alien_body_location[1] + 35, 0, 15, 20)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] + 75, Alien_body_location[1] + 20, 0, 27, 20)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] + 95, Alien_body_location[1] + 35, 0, 30, 17)
+# Left Leg
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] - 0, Alien_body_location[1] + 100, 0, 25, 40)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] - 10, Alien_body_location[1] + 130, 0, 20, 50)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] - 25, Alien_body_location[1] + 170, 0, 25, 25)
+# Rightleg
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] + 50, Alien_body_location[1] + 100, 0, 25, 40)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] + 65, Alien_body_location[1] + 130, 0, 20, 50)
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] + 75, Alien_body_location[1] + 170, 0, 25, 25)
+# Head
+alien_body_ellipse(Alien_location, color_alien, Alien_body_location[0] + 5, Alien_body_location[1] - 60, 0, 60, 60)
+alien_body_ellipse((Alien_location[0] - 20, Alien_location[1] - 130), color_alien, Alien_body_location[0], Alien_body_location[1], 60, 85, 35)
+alien_body_ellipse((Alien_location[0] - 70, Alien_location[1] - 160), color_alien, Alien_body_location[0], Alien_body_location[1], 120, 85, 35)
+alien_body_ellipse((Alien_location[0] - 10, Alien_location[1] - 87), color_alien, Alien_body_location[0], Alien_body_location[1], 0, 85, 35)
+# Eyes
+# Left
+alien_body_ellipse((Alien_location[0], Alien_location[1] - 65), color_black, Alien_body_location[0], Alien_body_location[1], 0, 25, 25)
+alien_body_ellipse((Alien_location[0] + 11, Alien_location[1] - 54), color_white, Alien_body_location[0], Alien_body_location[1], 0, 8, 8)
+# Right
+alien_body_ellipse((Alien_location[0] + 40, Alien_location[1] - 63), color_black, Alien_body_location[0], Alien_body_location[1], 0, 22, 22)
+alien_body_ellipse((Alien_location[0] + 51, Alien_location[1] - 54), color_white, Alien_body_location[0], Alien_body_location[1], 0, 8, 8)
+# Ears
+alien_body_ellipse((Alien_location[0] - 15, Alien_location[1] - 107), color_alien, Alien_body_location[0], Alien_body_location[1], 0, 15, 35)
+alien_body_ellipse((Alien_location[0] - 25, Alien_location[1] - 127), color_alien, Alien_body_location[0], Alien_body_location[1], 0, 25, 25)
+
+alien_body_ellipse((Alien_location[0] + 65, Alien_location[1] - 107), color_alien, Alien_body_location[0], Alien_body_location[1], 0, 15, 35)
+alien_body_ellipse((Alien_location[0] + 65, Alien_location[1] - 127), color_alien, Alien_body_location[0], Alien_body_location[1], 0, 25, 25)
+# Apple
+alien_body_ellipse((Alien_location[0] + 110, Alien_location[1] - 0), color_apple, Alien_body_location[0], Alien_body_location[1], 0, 45, 45)
+pi = 3.14
+alien_body_arc(Alien_location, color_black, Alien_body_location[0] + 130, Alien_body_location[1] - 20, 0, 40, 50, pi/2, pi, 2)
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
