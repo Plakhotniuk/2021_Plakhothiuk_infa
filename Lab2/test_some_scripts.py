d66d5fb89544f7dedf952b0c07c8cd9c2e93d1ac
Arseniy <plakhotniuk.av@phystech.edu>
2021-02-23 18:49:24 +0300

    some tests

--- Lab2/test_some_scripts.py
+++ Lab2/test_some_scripts.py
@@ -1,32 +1,348 @@
+# Imports
 import pygame
-from PIL import Image, ImageFilter
+import math
+import random
+
+# pylint: disable=no-member
+from pygame.constants import (
+    QUIT, KEYDOWN, K_l, K_r
+)
+
+# Initialize game engine
+
 
 pygame.init()
-screen = pygame.display.set_mode((512, 512))
+# pylint: enable=no-member
+
+
+# Window
+SIZE = (800, 600)
+TITLE = "Major League Soccer"
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption(TITLE)
+
+# Timer
 clock = pygame.time.Clock()
+refresh_rate = 60
 
-compressed = 'eJztnV2LHEUUhv0J+w+cf5CAELxQGT+IqNGsicYQds3kc4nEZDBiYhKTNmaFEBeyagzEiIMiKCIOKiiSi0HEGxU2F+qFKIMXGhTMXBiE3LT9jt1Db6c/qnqq6pyqroaXZGene2fqOXVO1anT1WEY3hIy0OLcpnak2UhBrF6kQY76qfd04vNa1J/fS4r1TMwOjFcihYoE+zgXX9vbBCPFzLuKeVdpJf6b3hbouLfifm6KeZF63g6Ms4cfHjFgn9Y56nZpguI4TM26LC7MULeRq1r8fxxPzdjbAB3/AQO+IupSt5WLitp1yICtiALqtnJRDLh6/p6/iAbUbeWiGHD1/D1/z7+B/F/atys8EQThseUL4dGL74RH3v1oIrz2wpmzYXD42fB0Z6vn7xB/MD36xqVVvKt0fGn5avRvi7q9XFLEYq3R/r5nftyvZbjnqOftQBn/tsk+//zb70/LPtEoEmk+KPr77UidSEFKXbxOzZUbf7BXxD2rIRgY4j0T8+4Lfja8b5aaMTV/jeyz/qAf98G2gFoS3FvxdUc1P9sKV5+gmz/ivUKfr0MrJXYTxL9X9bcCat6m+SsY67mmHjVzU/xPHdhXq40ufvnVRAx4OW0DOvkjdyPaJmc+/jz8/pdhmHfgdfyeATeVYlHXpJO/aNz/8Jvvcrlnj69/+pmamWq1XeUv6vtF2SfH5Ss/UjObKPjgk1WxCj9LXmPoKv+Tx44Ktd+/N25I8f/ht9/JucNmf/97lPv5fr36V7j82WWZ63WI+Xd18MeaTtV3R1+WPajHhEVjlOwBGxG8Zp+Yf6CD//Gl5crvXtSH8g74CWr2kMwhagMu8heZ94se8Pk1YqsWwb+LHrBZwc/d9vzzDwk/akSI7TJjlk+/vdJI/iLxv+rg4O+ntQH4C4FrBk3kX+ZLufX7rJCPEhm/wE6ayP/FZw5Wfu+iuT+nOX6ZROevTeSPdT+RNsz2IUF/yUaIUWUHvp/AdZyL/1C2njNPiKXX/rk+aSsu43wZleUE8Lum8j9xepGcjQmVxQGRMSwVe938UdtNzcaU8nKZgrnqFVf5i8YAFwQfkMQxHBKxLHCZv6HaPxYCb/gByblry2X+TfIBNTSgZG+Kf5N8gKQ6TeCPe/cYtDU3DanZm+LvY0Cu2tTsTfL3deCrRFrzkeFvZK9Hz38i3K/GZh+zRUN7f1Hx3/vy0kQHX3+Lmj3Ewu/bwP/QpffC+cMnws37DoQ7TsrlknHObevWhWvWrFmluzc8Ej61dJ6KfY+aNxV/2fEfGGX53XHf+rFNVNkM3pflnhXsioB/QM2bir9sW6Gf5nF7cNuTpefh91XsE8n6FM/fHP8iZrffeVfhOYjxouwh+Jcqf5Jcd/3mLeP3wy6nsJtG8q+T/yliBt9edA4YyfCHME6oYq/QdzSSf537gGX9P8b2suyr/EnZ56g6z/Ofjr/s+G9r97la/KGy+UDZeZ6/Pv8Pped/VeP1Or4/EXx80XWL5hKwTc9f3/hPVvDHdfnDdxRdF3ZXZ9xQoDY1byr+utd/6rIXYZnNJW3csbfu52wsf5H7Qan4m8oDULOm5C+yHwAV/7L4r1CkdZ7U/HXXAk/D39C6UI+aNSV/3WOAonl6lWqO4+uoQ82amr/OGFB3/l+1nqBQbNb8dfM/tXsu3LOwEN5z/wMTbXzs8fD47u3a9gKFD89b72US+9nU++jmj1rf/WdfG+fU0LaYP6fn5o9u38nGByBnZKjvs90DWiX/sjpvzJmnzJ0otQGRWgJFGlIzNsEfOd4q355mo9PvIhbA3vJyguBuuPajQ83YBH/RNZ6kPgOxmrAOy5QG1HxN8RfZ7wVK12c5bgN4VkCLmq8p/iL7vTTMBlj7fdX8Rfd7KbIBonrMRrNXyR+SfZ5btlZ3inU1TrKGvWr+MjEgbQPp3C3swdJ4MLKNvWr+UN113nR+IMkRGJqfqxCeEbSWmiUH/sgDyMaBRMgJpOfs+D/nccH8kZPXtzx96AI1Q078k7Fg3Tw/+jz6ftoXwA7wGpP798bcN8x1vti0sP9Wan4c+U/rByCwRq4orwYYeURD6zarfBNi1L0bN/1JzcwG/okNTFv3ldQAF9X36lpLAG9cO6+uYHbXwhPU3Gzgnwjr/qrWfZM1Rd0+oKyeuP3Qw39Qc7OJf+ILZJ4Hx0HJvgHwA4lQKxrbXYuanU3803YAfzDN2ICJetTsbOSfFuYJFtvCiJqd7fyztoD6Ed33CSgW27oe2/inpfteAYVi8QxX1/hDDNiKiOU9Hbbzl11LphQ1Pxf5W7ZfcJuaoWv8t27vjPNuEHLABPs0yahLzdAl/jvntuXm3GALDFjnKaBm6AJ/1A8ffuXVm3K6qAVBrpd438Yy9agZ2s7f8udEDagZ2szftjUBz1+dLBvne/4KJXK/mCXy/OvEfMH7hSyQ55/j1xHXMa5DTbjDfR9ie28/Bf+88Rz2fcGaDvK6NtaBVCigZsiFv+XzuLqyeg1YJX+HfLqMWtQMufBnwMK0htT8OPFv4DP+fP1HOv67M6cTlZX3/OniP77npzk+YEDNjht/CPu8NmQc2KZmx5F/Q2zAib6vi38DbMD6uK+b/9gG9szbem9HmQJqZrbwT+RQXnBAzctG/hDquyyfG2CPF5Z7eNvAH8L80NIcgZPsTfNPjwssus/PWfYx/75p/umYUPVceIwf6zw7XpF6LrOP+QdU/Cf+IJorYowIzonwM15P+4xj59+8Zog79vKzel3XJv4SGsR9Uif3wPU+bzN/fOaITzdmpYr7sGncbeYf28BMzGw4BfNeU/y8a/zTihiujX0CeA4K1I/tZfaI5TU7nr+X5+/l+Xt5/l6ev9cU/GcZcPX8aW2gx4CtiKy+146zFvnHgVEkZ+quOCpq39Yig/1gcgT/1LjcLKEdtJnEBNhim7o9mir0uUhdwz4Bfv4cfBH19/e6yRYwVwhiexgp7ueB7+v2KY4V7Zhfon7MNKte6j04x4/nJPUfe7dmpQ=='
-size, image_mode, raw = (128, 128), 'RGBA', compressed.decode("base64").decode("zlib")
+# Colors
+''' add colors you use as RGB values here '''
+RED = (255, 0, 0)
+GREEN = (52, 166, 36)
+BLUE = (29, 116, 248)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ORANGE = (255, 125, 0)
+DARK_BLUE = (18, 0, 91)
+DARK_GREEN = (0, 94, 0)
+GRAY = (130, 130, 130)
+YELLOW = (255, 255, 110)
+SILVER = (200, 200, 200)
+DAY_GREEN = (41, 129, 29)
+NIGHT_GREEN = (0, 64, 0)
+BRIGHT_YELLOW = (255, 244, 47)
+NIGHT_GRAY = (104, 98, 115)
+ck = (127, 33, 33)
 
-# create the original pygame surface
-surf = pygame.image.fromstring(raw, size, image_mode)
+DARKNESS = pygame.Surface(SIZE)
+DARKNESS.set_alpha(200)
+DARKNESS.fill((0, 0, 0))
 
-# create a PIL image and blur it
-pil_blured = Image.fromstring("RGBA", size, raw).filter(ImageFilter.GaussianBlur(radius=6))
+SEE_THROUGH = pygame.Surface((800, 180))
+SEE_THROUGH.set_alpha(150)
+SEE_THROUGH.fill((124, 118, 135))
 
-# convert it back to a pygame surface
-other = pygame.image.fromstring(pil_blured.tostring("raw", image_mode), size, image_mode)
 
-pygame.time.set_timer(pygame.USEREVENT, 1000)
+def draw_cloud(x, y):
+    pygame.draw.ellipse(SEE_THROUGH, cloud_color, [x, y + 8, 10, 10])
+    pygame.draw.ellipse(SEE_THROUGH, cloud_color, [x + 6, y + 4, 8, 8])
+    pygame.draw.ellipse(SEE_THROUGH, cloud_color, [x + 10, y, 16, 16])
+    pygame.draw.ellipse(SEE_THROUGH, cloud_color, [x + 20, y + 8, 10, 10])
+    pygame.draw.rect(SEE_THROUGH, cloud_color, [x + 6, y + 8, 18, 10])
 
-while True:
-    for e in pygame.event.get():
-        if e.type == pygame.USEREVENT: surf, other = other, surf
-        if e.type == pygame.QUIT: break
+
+# Config
+lights_on = True
+day = True
+
+stars = []
+for n in range(200):
+    x = random.randrange(0, 800)
+    y = random.randrange(0, 200)
+    r = random.randrange(1, 2)
+    stars.append([x, y, r, r])
+
+clouds = []
+for i in range(20):
+    x = random.randrange(-100, 1600)
+    y = random.randrange(0, 150)
+    clouds.append({"x": x, "y": y})
+
+# Game loop
+done = False
+
+while not done:
+    # Event processing (React to key presses, mouse clicks, etc.)
+    ''' for now, we'll just check to see if the X is clicked '''
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            done = True
+        elif event.type == KEYDOWN:
+            if event.key == K_l:
+                lights_on = not lights_on
+            elif event.key == K_r:
+                day = not day
+                print(day)
+
+    # Game logic (Check for collisions, update points, etc.)
+    ''' leave this section alone for now '''
+    if lights_on:
+        light_color = YELLOW
     else:
-        screen.fill((255, 255, 255))
-        screen.blit(surf, (192, 192))
-        pygame.display.flip()
-        clock.tick(60)
-        continue
-    break
+        light_color = SILVER
+
+    if day:
+        sky_color = BLUE
+        field_color = GREEN
+        stripe_color = DAY_GREEN
+        cloud_color = WHITE
+    else:
+        sky_color = DARK_BLUE
+        field_color = DARK_GREEN
+        stripe_color = NIGHT_GREEN
+        cloud_color = NIGHT_GRAY
+
+    for c in clouds:
+        c['x'] -= 0.5
+
+        if c['x'] < -100:
+            c['x'] = random.randrange(800, 1600)
+            c['y'] = random.randrange(0, 150)
+
+    # Drawing code (Describe the picture. It isn't actually drawn yet.)
+    screen.fill(sky_color)
+    SEE_THROUGH.fill(ck)
+    SEE_THROUGH.set_colorkey(ck)
+
+    if not day:
+        # stars
+        for s in stars:
+            pygame.draw.ellipse(screen, WHITE, s)
+
+    pygame.draw.rect(screen, field_color, [0, 180, 800, 420])
+    pygame.draw.rect(screen, stripe_color, [0, 180, 800, 42])
+    pygame.draw.rect(screen, stripe_color, [0, 264, 800, 52])
+    pygame.draw.rect(screen, stripe_color, [0, 368, 800, 62])
+    pygame.draw.rect(screen, stripe_color, [0, 492, 800, 82])
+
+    '''fence'''
+    y = 170
+    for x in range(5, 800, 30):
+        pygame.draw.polygon(screen, NIGHT_GRAY, [[x + 2, y], [x + 2, y + 15], [x, y + 15], [x, y]])
+
+    y = 170
+    for x in range(5, 800, 3):
+        pygame.draw.line(screen, NIGHT_GRAY, [x, y], [x, y + 15], 1)
+
+    x = 0
+    for y in range(170, 185, 4):
+        pygame.draw.line(screen, NIGHT_GRAY, [x, y], [x + 800, y], 1)
+
+    if day:
+        pygame.draw.ellipse(screen, BRIGHT_YELLOW, [520, 50, 40, 40])
+    else:
+        pygame.draw.ellipse(screen, WHITE, [520, 50, 40, 40])
+        pygame.draw.ellipse(screen, sky_color, [530, 45, 40, 40])
+
+    for c in clouds:
+        draw_cloud(c['x'], c['y'])
+    screen.blit(SEE_THROUGH, (0, 0))
+
+    # out of bounds lines
+    pygame.draw.line(screen, WHITE, [0, 580], [800, 580], 5)
+    # left
+    pygame.draw.line(screen, WHITE, [0, 360], [140, 220], 5)
+    pygame.draw.line(screen, WHITE, [140, 220], [660, 220], 3)
+    # right
+    pygame.draw.line(screen, WHITE, [660, 220], [800, 360], 5)
+
+    # safety circle
+    pygame.draw.ellipse(screen, WHITE, [240, 500, 320, 160], 5)
+
+    # 18 yard line goal box
+    pygame.draw.line(screen, WHITE, [260, 220], [180, 300], 5)
+    pygame.draw.line(screen, WHITE, [180, 300], [620, 300], 3)
+    pygame.draw.line(screen, WHITE, [620, 300], [540, 220], 5)
+
+    # arc at the top of the goal box
+    pygame.draw.arc(screen, WHITE, [330, 280, 140, 40], math.pi, 2 * math.pi, 5)
+
+    # score board pole
+    pygame.draw.rect(screen, GRAY, [390, 120, 20, 70])
+
+    # score board
+    pygame.draw.rect(screen, BLACK, [300, 40, 200, 90])
+    pygame.draw.rect(screen, WHITE, [302, 42, 198, 88], 2)
+
+    # goal
+    pygame.draw.rect(screen, WHITE, [320, 140, 160, 80], 5)
+    pygame.draw.line(screen, WHITE, [340, 200], [460, 200], 3)
+    pygame.draw.line(screen, WHITE, [320, 220], [340, 200], 3)
+    pygame.draw.line(screen, WHITE, [480, 220], [460, 200], 3)
+    pygame.draw.line(screen, WHITE, [320, 140], [340, 200], 3)
+    pygame.draw.line(screen, WHITE, [480, 140], [460, 200], 3)
+
+    # 6 yard line goal box
+    pygame.draw.line(screen, WHITE, [310, 220], [270, 270], 3)
+    pygame.draw.line(screen, WHITE, [270, 270], [530, 270], 2)
+    pygame.draw.line(screen, WHITE, [530, 270], [490, 220], 3)
+
+    # light pole 1
+    pygame.draw.rect(screen, GRAY, [150, 60, 20, 140])
+    pygame.draw.ellipse(screen, GRAY, [150, 195, 20, 10])
+
+    # lights
+    pygame.draw.line(screen, GRAY, [110, 60], [210, 60], 2)
+    pygame.draw.ellipse(screen, light_color, [110, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [130, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [150, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [170, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [190, 40, 20, 20])
+    pygame.draw.line(screen, GRAY, [110, 40], [210, 40], 2)
+    pygame.draw.ellipse(screen, light_color, [110, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [130, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [150, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [170, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [190, 20, 20, 20])
+    pygame.draw.line(screen, GRAY, [110, 20], [210, 20], 2)
+
+    # light pole 2
+    pygame.draw.rect(screen, GRAY, [630, 60, 20, 140])
+    pygame.draw.ellipse(screen, GRAY, [630, 195, 20, 10])
+
+    # lights
+
+    pygame.draw.line(screen, GRAY, [590, 60], [690, 60], 2)
+    pygame.draw.ellipse(screen, light_color, [590, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [610, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [630, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [650, 40, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [670, 40, 20, 20])
+    pygame.draw.line(screen, GRAY, [590, 40], [690, 40], 2)
+    pygame.draw.ellipse(screen, light_color, [590, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [610, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [630, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [650, 20, 20, 20])
+    pygame.draw.ellipse(screen, light_color, [670, 20, 20, 20])
+    pygame.draw.line(screen, GRAY, [590, 20], [690, 20], 2)
+
+    # net
+    pygame.draw.line(screen, WHITE, [325, 140], [341, 200], 1)
+    pygame.draw.line(screen, WHITE, [330, 140], [344, 200], 1)
+    pygame.draw.line(screen, WHITE, [335, 140], [347, 200], 1)
+    pygame.draw.line(screen, WHITE, [340, 140], [350, 200], 1)
+    pygame.draw.line(screen, WHITE, [345, 140], [353, 200], 1)
+    pygame.draw.line(screen, WHITE, [350, 140], [356, 200], 1)
+    pygame.draw.line(screen, WHITE, [355, 140], [359, 200], 1)
+    pygame.draw.line(screen, WHITE, [360, 140], [362, 200], 1)
+    pygame.draw.line(screen, WHITE, [364, 140], [365, 200], 1)
+    pygame.draw.line(screen, WHITE, [368, 140], [369, 200], 1)
+    pygame.draw.line(screen, WHITE, [372, 140], [373, 200], 1)
+    pygame.draw.line(screen, WHITE, [376, 140], [377, 200], 1)
+    pygame.draw.line(screen, WHITE, [380, 140], [380, 200], 1)
+    pygame.draw.line(screen, WHITE, [384, 140], [384, 200], 1)
+    pygame.draw.line(screen, WHITE, [388, 140], [388, 200], 1)
+    pygame.draw.line(screen, WHITE, [392, 140], [392, 200], 1)
+    pygame.draw.line(screen, WHITE, [396, 140], [396, 200], 1)
+    pygame.draw.line(screen, WHITE, [400, 140], [400, 200], 1)
+    pygame.draw.line(screen, WHITE, [404, 140], [404, 200], 1)
+    pygame.draw.line(screen, WHITE, [408, 140], [408, 200], 1)
+    pygame.draw.line(screen, WHITE, [412, 140], [412, 200], 1)
+    pygame.draw.line(screen, WHITE, [416, 140], [416, 200], 1)
+    pygame.draw.line(screen, WHITE, [420, 140], [420, 200], 1)
+    pygame.draw.line(screen, WHITE, [424, 140], [423, 200], 1)
+    pygame.draw.line(screen, WHITE, [428, 140], [427, 200], 1)
+    pygame.draw.line(screen, WHITE, [432, 140], [431, 200], 1)
+    pygame.draw.line(screen, WHITE, [436, 140], [435, 200], 1)
+    pygame.draw.line(screen, WHITE, [440, 140], [438, 200], 1)
+    pygame.draw.line(screen, WHITE, [445, 140], [441, 200], 1)
+    pygame.draw.line(screen, WHITE, [450, 140], [444, 200], 1)
+    pygame.draw.line(screen, WHITE, [455, 140], [447, 200], 1)
+    pygame.draw.line(screen, WHITE, [460, 140], [450, 200], 1)
+    pygame.draw.line(screen, WHITE, [465, 140], [453, 200], 1)
+    pygame.draw.line(screen, WHITE, [470, 140], [456, 200], 1)
+    pygame.draw.line(screen, WHITE, [475, 140], [459, 200], 1)
+
+    # net part 2
+    pygame.draw.line(screen, WHITE, [320, 140], [324, 216], 1)
+    pygame.draw.line(screen, WHITE, [320, 140], [326, 214], 1)
+    pygame.draw.line(screen, WHITE, [320, 140], [328, 212], 1)
+    pygame.draw.line(screen, WHITE, [320, 140], [330, 210], 1)
+    pygame.draw.line(screen, WHITE, [320, 140], [332, 208], 1)
+    pygame.draw.line(screen, WHITE, [320, 140], [334, 206], 1)
+    pygame.draw.line(screen, WHITE, [320, 140], [336, 204], 1)
+    pygame.draw.line(screen, WHITE, [320, 140], [338, 202], 1)
+
+    # net part 3
+    pygame.draw.line(screen, WHITE, [480, 140], [476, 216], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [474, 214], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [472, 212], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [470, 210], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [468, 208], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [466, 206], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [464, 204], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [462, 202], 1)
+
+    # net part 4
+    pygame.draw.line(screen, WHITE, [324, 144], [476, 144], 1)
+    pygame.draw.line(screen, WHITE, [324, 148], [476, 148], 1)
+    pygame.draw.line(screen, WHITE, [324, 152], [476, 152], 1)
+    pygame.draw.line(screen, WHITE, [324, 156], [476, 156], 1)
+    pygame.draw.line(screen, WHITE, [324, 160], [476, 160], 1)
+    pygame.draw.line(screen, WHITE, [324, 164], [476, 164], 1)
+    pygame.draw.line(screen, WHITE, [324, 168], [476, 168], 1)
+    pygame.draw.line(screen, WHITE, [324, 172], [476, 172], 1)
+    pygame.draw.line(screen, WHITE, [324, 176], [476, 176], 1)
+    pygame.draw.line(screen, WHITE, [335, 180], [470, 180], 1)
+    pygame.draw.line(screen, WHITE, [335, 184], [465, 184], 1)
+    pygame.draw.line(screen, WHITE, [335, 188], [465, 188], 1)
+    pygame.draw.line(screen, WHITE, [335, 192], [465, 192], 1)
+    pygame.draw.line(screen, WHITE, [335, 196], [465, 196], 1)
+
+    # stands right
+    pygame.draw.polygon(screen, RED, [[680, 220], [800, 340], [800, 290], [680, 180]])
+    pygame.draw.polygon(screen, WHITE, [[680, 180], [800, 100], [800, 290]])
+
+    # stands left
+    pygame.draw.polygon(screen, RED, [[120, 220], [0, 340], [0, 290], [120, 180]])
+    pygame.draw.polygon(screen, WHITE, [[120, 180], [0, 100], [0, 290]])
+    # people
+
+    # corner flag right
+    pygame.draw.line(screen, BRIGHT_YELLOW, [140, 220], [135, 190], 3)
+    pygame.draw.polygon(screen, RED, [[132, 190], [125, 196], [135, 205]])
+
+    # corner flag left
+    pygame.draw.line(screen, BRIGHT_YELLOW, [660, 220], [665, 190], 3)
+    pygame.draw.polygon(screen, RED, [[668, 190], [675, 196], [665, 205]])
+
+    # DARKNESS
+    if not day and not lights_on:
+        screen.blit(DARKNESS, (0, 0))
+
+        # pygame.draw.polygon(screen, BLACK, [[200, 200], [50,400], [600, 500]], 10)
+
+    ''' angles for arcs are measured in radians (a pre-cal topic) '''
+    # pygame.draw.arc(screen, ORANGE, [100, 100, 100, 100], 0, math.pi/2, 1)
+    # pygame.draw.arc(screen, BLACK, [100, 100, 100, 100], 0, math.pi/2, 50)
+
+    # Update screen (Actually draw the picture in the window.)
+    pygame.display.flip()
+
+    # Limit refresh rate of game loop
+    clock.tick(refresh_rate)
+
+# Close window and quit
+# pylint: disable=no-member
+pygame.quit()
+# pylint: enable=no-member
