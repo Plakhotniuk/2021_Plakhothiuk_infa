b5cfb7fe9872e2b121806db6402bf8b31d6a3c89
Arseniy Plakhotniuk <plakhotniuk.av@phystech.edu>
2021-03-24 23:30:56 +0300

    Finished

--- Game_Gun/My_Game_Gun.py
+++ Game_Gun/My_Game_Gun.py
@@ -1,13 +1,15 @@
 import pygame
 from random import randint
 import math
-import time
 
 pygame.init()
 
-FPS = 60
+FPS = 70
 screensize = (1200, 800)
 screen = pygame.display.set_mode(screensize, pygame.SRCALPHA)
+
+pygame.display.update()
+clock = pygame.time.Clock()
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -25,73 +27,76 @@ targets_mass = {'massive': [], 'remove ind': -1}
 
 
 class Player:
-    def __init__(self, username='', score=0, shoots=0):
+
+    def __init__(self, username='', score=0, shots=0):
+        """
+        Some information about player (Score, Username, number of shots)
+        :param username:
+        :param score:
+        :param shots:
+        """
         self.score = score
         self.username = username
-        self.shoots = shoots
-
-    def set_username(self, username):
-        self.username = username
-
-    def set_score(self, points):
-        self.score += points
-
-    def get_username(self):
-        return self.username
-
-    def get_score(self):
-        return self.score
-
-    def get_shoots(self):
-        return self.shoots
-
-    def set_shoots(self):
-        self.shoots += 1
+        self.shots = shots
 
 
 class Ball:
+    """
+    Class of balls (Shells)
+    """
     def __init__(self):
+        """
+        Generates random parameters of Ball (color, coord, velocity)
+        Default parameters: gravitation, health, is_alive
+        """
         self.color = COLORS[randint(0, 5)]
         self.radius = randint(30, 50)
         self.coord_x = randint(self.radius, screensize[0] - self.radius)
         self.coord_y = randint(self.radius, screensize[1] - self.radius)
         self.velocity_x = int(randint(-5, 5))
         self.velocity_y = int(randint(-5, 5))
-        self.gravitation = 0
+        self.gravitation = 0.2
         self.health = 30
         self.is_alive = True
 
     def draw_ball(self):
         """
-        :return: Draws ball
+        Draws ball
         """
         pygame.draw.circle(screen, self.color, (self.coord_x, self.coord_y), self.radius)
 
     def move_ball(self):
         """
-        :return: Moves ball
+        Moves ball
         """
         self.coord_x += self.velocity_x
         self.coord_y -= self.velocity_y
         self.velocity_y -= self.gravitation
 
-        if self.coord_x >= screensize[0] - self.radius or self.coord_x <= self.radius:
+        if self.coord_x > screensize[0] - self.radius or self.coord_x < self.radius:
             self.velocity_x *= -0.9
-            self.health -= 2
-        if self.coord_y >= screensize[1] - 50:
+            self.health -= 1
+        if self.coord_y > screensize[1] - 50:
             self.velocity_y *= -0.9
-            self.health -= 2
-        if abs(self.velocity_x) < 0.05:
-            self.velocity_x = 0
-        if abs(self.velocity_y) < 0.05:
+            self.health -= 1
+        if abs(self.velocity_y) < 0.001:
             self.velocity_y = 0
-        self.draw_ball()
         if self.health <= 0:
             self.is_alive = False
+        self.draw_ball()
+        pygame.draw.circle(screen, BLACK, (self.coord_x, self.coord_y), self.radius, width=2)
 
 
 class BallTarget(Ball):
+    """
+    Class of Targets, inheritance from Balls
+    """
     def __init__(self, health=30, is_alive=True):
+        """
+        Generates new Target
+        :param health: health points
+        :param is_alive: Default (True)
+        """
         super().__init__()
         self.health = health
         self.is_alive = is_alive
@@ -99,7 +104,6 @@ class BallTarget(Ball):
     def move_target(self):
         """
         Moves targets
-        :return: motion
         """
         self.coord_x += self.velocity_x
         self.coord_y += self.velocity_y
@@ -113,30 +117,45 @@ class BallTarget(Ball):
 
     def hit_enemy(self, obj, player):
         """
-        :param obj:Aim
-        :return: Result of hit test
+        Updates health points of target and adds points of player, if he hits
+        :param player: Player
+        :param obj: Aim
         """
         if (self.coord_x - obj.coord_x) ** 2 + (self.coord_y - obj.coord_y) ** 2 <= (self.radius + obj.radius) ** 2:
             self.health -= 100
-
             player.score += 1
             if self.health <= 0:
                 self.is_alive = False
 
 
 class Gun:
-    def __init__(self, x=40, y=500, color=BLACK, length=70, angle=0, shoot_mode=False, power=0):
+    """
+    Class of Cannon
+    """
+    def __init__(self, x=40, y=500, color=BLACK, length=70, angle=0, shooting_mode=False, power=0):
+        """
+        :param x: coordinate x
+        :param y: coordinate y
+        :param color: color
+        :param length: gun barrel length
+        :param angle: angle with the horizon
+        :param shooting_mode:
+        :param power:
+        """
         self.x = x
         self.y = y
         self.color = color
         self.length = length
         self.angle = angle
-        self.shoot_mode = shoot_mode
+        self.shooting_mode = shooting_mode
         self.power = power
         self.maxlength = length*1.5
 
     def fire2_start(self):
-        self.shoot_mode = True
+        """
+        Starts shooting mode
+        """
+        self.shooting_mode = True
 
     def fire2_end(self):
         """Выстрел мячом.
@@ -144,7 +163,7 @@ class Gun:
         Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
         """
         new_ball = Ball()
-        new_ball.radius = 10
+        new_ball.radius = 15
 
         x0, y0 = pygame.mouse.get_pos()
 
@@ -152,8 +171,8 @@ class Gun:
             pass
         else:
             self.angle = -math.atan((y0 - self.y) / (x0 - self.x))
-        new_ball.velocity_x = self.power * math.cos(self.angle)
-        new_ball.velocity_y = self.power * math.sin(self.angle)
+        new_ball.velocity_x = self.power * math.cos(self.angle) * 0.5
+        new_ball.velocity_y = self.power * math.sin(self.angle) * 0.5
 
         new_ball.coord_x = self.x + self.length * math.cos(self.angle)
         new_ball.coord_y = self.y - self.length * math.sin(self.angle)
@@ -161,19 +180,26 @@ class Gun:
         self.power = 0
         self.length = 70
         self.color = BLACK
-        self.shoot_mode = False
+        self.shooting_mode = False
 
-    def draw_gan(self):
+    def draw_gun(self):
+        """
+        Draws Gun
+        """
         pygame.draw.line(screen, self.color, (self.x, self.y),
-                         (self.x + self.length * math.cos(self.angle), self.y - self.length * math.sin(self.angle)), width=7)
+                         (self.x + self.length * math.cos(self.angle),
+                          self.y - self.length * math.sin(self.angle)), width=7)
 
     def targeting(self):
+        """
+        Process of targeting (depends on mouse position)
+        """
         x0, y0 = pygame.mouse.get_pos()
         if x0 == self.x:
             pass
         else:
             self.angle = -math.atan((y0 - self.y) / (x0 - self.x))
-        if self.shoot_mode:
+        if self.shooting_mode:
             self.color = RED
             if self.length >= self.maxlength:
                 pass
@@ -182,10 +208,13 @@ class Gun:
         else:
             self.color = BLACK
 
-        self.draw_gan()
+        self.draw_gun()
 
     def power_up(self):
-        if self.shoot_mode:
+        """
+        Gains power of shot
+        """
+        if self.shooting_mode:
             if self.power < 100:
                 self.power += 0.5
             self.color = RED
@@ -194,41 +223,43 @@ class Gun:
             self.color = BLACK
 
 
-
-pygame.display.update()
-clock = pygame.time.Clock()
-
-
 def generate_targets(number_of_targets):
+    """
+    Generation of targets, put them in massive
+    :param number_of_targets:set number of targets
+    """
     for i in range(number_of_targets):
         targets_mass['massive'].append(BallTarget())
 
+
 finished = False
+
 
 def game():
     """
-    :return:Game
+    Game session process
     """
     global finished
-    generate_targets(5)
+    generate_targets(2)
     counter = 0
     new_gun = Gun()
     finished_session = False
-    finished_session1 = False
+    see_results = False
     new_player = Player()
     element_mass['massive'].clear()
+
     while not finished_session:
 
         clock.tick(FPS)
-        if not finished_session1:
+        if not see_results:
             myfont = pygame.font.SysFont('Times New Roman', 30)
-            scoretext = myfont.render((str(new_player.shoots)), True, BLACK)
+            scoretext = myfont.render((str(new_player.shots)), True, BLACK)
             screen.blit(scoretext, (screensize[0] // 8, 30))
-        if finished_session1:
+        if see_results:
             myfont2 = pygame.font.SysFont('Times New Roman', 30)
-            finish_session_text = myfont2.render(("Вы уничтожили все цели за: " + str(new_player.shoots)),
+            finish_session_text = myfont2.render(("Вы уничтожили все цели за: " + str(new_player.shots) + " выстрелов"),
                                                  True, BLACK)
-            screen.blit(finish_session_text, (screensize[0] // 2, screensize[1] // 2))
+            screen.blit(finish_session_text, (screensize[0] // 3.5, screensize[1] // 3))
             counter += 1
             if counter == 150:
                 finished_session = True
@@ -242,7 +273,8 @@ def game():
                 new_gun.fire2_start()
             elif event.type == pygame.MOUSEBUTTONUP:
                 new_gun.fire2_end()
-                new_player.shoots += 1
+                if not see_results:
+                    new_player.shots += 1
 
         new_gun.targeting()
         new_gun.power_up()
@@ -272,7 +304,7 @@ def game():
             element_mass['massive'].remove(element_mass['massive'][element_mass['remove ind']])
 
         if not targets_mass['massive']:
-            finished_session1 = True
+            see_results = True
 
         pygame.display.update()
         screen.fill(WHITE)
